fad46ce1a2cac8aa9b50d0ede5168e7d4d147691
intan2584 <intandewi2584@gmail.com>
2024-12-30 23:40:06 +0800

    tambah function login
    buat function login di server.js dan login data berhasil

---
+++ api/server.js
@@ -0,0 +1,42 @@
+// Fungsi untuk memanggil API GET
+
+let base_url = "https://primdev.alwaysdata.net/api";
+
+// Fungsi untuk memanggil API POST
+async function login(email, password) {
+  // console.log('base url!!'+ base_url);
+
+  const response = await fetch(base_url + "/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  const data = await response.json();
+  return data;
+}
+// Fungsi untuk register API POST
+async function register() {
+  // console.log('base url!!'+ base_url);
+
+  const response = await fetch(base_url + "/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      password: password,
+      confirm_password: confirmPassword,
+    }),
+  });
+
+  const data = await response.json();
+  return data;
+}
+
+// Ekspor fungsi untuk digunakan di file HTML
+export { login, register };
