99745fb11c547a6d4900d1ed94dc8bd8cfb4ff2a
Bagusgede <56815571+Bagusgede@users.noreply.github.com>
2025-01-03 20:48:34 +0800

    update return server.js

--- api/server.js
+++ api/server.js
@@ -2,6 +2,25 @@
 
 let base_url = "https://primdev.alwaysdata.net/api";
 
+
+// Fungsi untuk memanggil API POST
+
+async function loggedUser(token) { 
+  // console.log('base url!!'+ base_url);
+  
+  const response = await fetch(base_url+'/user', {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+      },
+  });
+
+  const data = await response.json();
+
+  return data;
+}
+
 // Fungsi untuk memanggil API POST
 async function login(email, password) {
   // console.log('base url!!'+ base_url);
@@ -14,24 +33,25 @@ async function login(email, password) {
     body: JSON.stringify({ email, password }),
   });
 
-  const data = await response.json();
-  return data;
+  const responses = await response.json();
+  return responses;
 }
-// Fungsi untuk register API POST
-async function register() {
-  // console.log('base url!!'+ base_url);
 
-  const response = await fetch(base_url + "/register", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: name,
-      email: email,
-      password: password,
-      confirm_password: confirmPassword,
-    }),
+// Fungsi untuk register API POST
+async function register(name,email,password,confirmPassword) { 
+  // console.log('base url!!'+ base_url);
+  
+  const response = await fetch(base_url+'/register', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          'name' : name,
+          'email':email,
+          'password' : password, 
+          'confirm_password' : confirmPassword 
+      }),
   });
 
   const data = await response.json();
@@ -39,4 +59,4 @@ async function register() {
 }
 
 // Ekspor fungsi untuk digunakan di file HTML
-export { login, register };
+export { login, register, loggedUser };
